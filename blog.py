f18aa33b00556592e349d947a8daf725e2aaa5af
余国强 <yiguoqian@gmail.com>
2012-10-09 18:09:31 +0800

    添加登陆功能

--- blog.py
+++ blog.py
@@ -1,5 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash,jsonify
+
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
 frontend = Flask(__name__)
+frontend.config.from_object(__name__)
 
 @frontend.route('/')
 def index():
@@ -21,5 +30,17 @@ def material():
 def login():
     return render_template('frontend/login.html')
 
+@frontend.route('/user/')
+def user():
+    name = request.args.get('name')
+    password = request.args.get('password')
+    if name != frontend.config['USERNAME']:
+        error = 'Invalid password'
+    elif password != frontend.config['PASSWORD']:
+        error = 'Invalid password'
+    else:
+        return redirect(url_for('index'))
+    #return render_template('frontend/login.html')
+
 if __name__ == "__main__":
-    frontend.run(debug = True)
+    frontend.run()
